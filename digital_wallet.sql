0b78c27375f84e7510848148b6b3eadd5f48f463
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-06-21 23:01:42 +0300

    Created the database with users and transactions tables

---
+++ digital_wallet.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS digital_wallet;
+USE digital_wallet;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    address TEXT,
+    contact VARCHAR(20),
+    balance DECIMAL(10,2) DEFAULT 0.00,
+    daily_limit DECIMAL(10,2) DEFAULT 1000.00,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    type ENUM('deposit', 'withdraw') NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
